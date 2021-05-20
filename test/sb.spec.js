f8304d83956a7212e092466b48aa0516a00185d0
Mac-lp3 <staticlp3@gmail.com>
2021-05-20 15:25:58 +1200

    typescript stuff

---
+++ test/sb.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var got_1 = __importDefault(require("got"));
+var defaultHeaders = {
+    Authorization: "Bearer User Token",
+    X: X
+} - Akahu - ID;
+"App Token";
+;
+describe('something', function () {
+    it('should', function () {
+        console.log('yip');
+        got_1.default().
+        ;
+    });
+});
--- test/sb.spec.ts
+++ test/sb.spec.ts
@@ -1,6 +1,14 @@
+import got from 'got';
+
+const defaultHeaders: any = {
+    "Authorization": `Bearer User Token`,
+    "X-Akahu-ID": `App Token`
+};
+
 describe('something', function() {
 
     it('should', function() {
         console.log('yip');
+        
     })
 })
